827117ea76f74146b3a9d2e49f0a44f4e9a5f326
Sahandevs <sahandevs@gmail.com>
2019-08-06 10:58:41 +0430

    setup example project

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import FLMC from "flmc-lite-renderer";
+import FLMC, { FormController, Label } from "flmc-lite-renderer";
+
+class SampleForm extends FormController {
+  elements = [Label("This is a test label")];
+}
+
+const categoties = {
+  root: {
+    name: "Root",
+    hidden: false
+  }
+};
+
+const routes = [
+  {
+    path: "/",
+    builder: (path, params) => new SampleForm(),
+    category: categoties.root,
+    name: "Home",
+    hidden: false
+  }
+];
 
 function App() {
-  return <FLMC />;
+  return <FLMC routes={routes} />;
 }
 
 export default App;
